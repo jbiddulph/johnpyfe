d4a5000ab5a4bc7f55e2ff12777c0d4a29159a73
jbiddulph <john.mbiddulph@gmail.com>
2024-05-19 11:27:56 +0100

    set user admin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,11 +6,12 @@ export default defineNuxtConfig({
   // ],
   runtimeConfig: {
     public: {
-      appURL: process.env.NUXT_PUBLIC_APP_URL ?? "http://lookwhatfound.me",
+      appURL: process.env.NUXT_PUBLIC_APP_URL ?? "https://lookwhatfound.me",
       apiURL: process.env.NUXT_PUBLIC_API_URL ?? "https://lookwhatfound.me",
       googleMaps: {
         key: 'AIzaSyBWiPC71sMkSoaa0TNrioE8CP1Ll5HmpZ4'
       },
+      admin: process.env.ADMIN_EMAIL,
       // apiURL: process.env.NUXT_PUBLIC_API_URL ?? "http://lookwhatfound.me",
       mapbox_token:
             "pk.eyJ1IjoiamJpZGR1bHBoIiwiYSI6ImNscDgzemt0ZzJjNW8ydnM0MXJvNG56NjEifQ.h0CNNEv-Yjgkp4WMjOK9mA",
