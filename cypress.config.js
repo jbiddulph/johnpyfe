07fe3949ebde8df14d3b59364b71ff267e8208d7
jbiddulph <john.mbiddulph@gmail.com>
2024-05-21 23:07:51 +0100

    changed cypress back to localhost

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: `${useRuntimeConfig().public.baseURL}`,
+    baseUrl: `http://localhost:3000`,
   },
   setupNodeEvents(on, config) {
     // implement node event listeners here
