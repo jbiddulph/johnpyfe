f62d7fc5a1c214a278c6080f30453c6fd4e9205a
jbiddulph <john.mbiddulph@gmail.com>
2024-05-21 23:14:57 +0100

    cors url changed

--- middleware/cors.js
+++ middleware/cors.js
@@ -2,7 +2,7 @@ import cors from 'cors';
 
 // Configure CORS middleware
 export default cors({
-  origin: 'http://127.0.0.1:3000', // Allow requests from your frontend application
+  origin: 'http://localhost:3000', // Allow requests from your frontend application
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
   allowedHeaders: ['Content-Type'], // Allow specific headers
 });
