d204e52456d7385a637e2c79c209d557bddabd61
jbiddulph <john.mbiddulph@gmail.com>
2024-08-25 23:50:52 +0100

    added countdown to the events list

--- pages/Events.vue
+++ pages/Events.vue
@@ -13,26 +13,21 @@
         <UButton label="Next" @click="nextPage(currentPage + 1)" />
         <!-- <UButton label="Last" @click="nextPage(currentPage = totalPages)" /> -->
       </div>
-      <ul class="grid grid-cols-1 sm:grid-cols-2 gap-4">
-    <!-- <li v-for="(event, index) in paginatedEvents" :key="index"> -->
-      <li v-for="(event, index) in events" :key="index">
-        <div class="flex items-center bg-white dark:bg-gray-900"> <!-- Flex container to align items horizontally -->
-          <img class="w-250 h-auto mr-4" :src="`${config.public.supabase.url}/storage/v1/object/public/event_images/${event.photo}`" alt="Event image" width="250px" /> <!-- Image -->
-          <div class="flex content-between flex-wrap"> <!-- Description -->
-              <h3 class="font-bold text-black dark:text-white">{{ event.event_title }} at Venue: {{ event.venue }}</h3>
-                <div class="text-black dark:text-white">{{ event.event_date }}, {{ event.time_start }} - {{ event.time_end }}</div>
-                <div class="flex justify-center">
-                    <UButton label="Details" class="mr-2" @click="openDetailsModal(event)" />
-                    <div v-if="userName === user.user_metadata.name">
-                        <UButton label="Edit" class="mr-2" color="amber" @click="openEditModal(event, event.id)" />
-                        <UButton label="Delete" color="red" @click="openDeleteModal(event, event.id)" />
-                        <UButton label="Event" color="green" @click="openAddEventModal(event, event.id)" />
-                    </div>
+      <ul class="grid grid-cols-2 sm:grid-cols-4 gap-4">
+          <li v-for="(event, index) in events" :key="index">
+              <div class="w-full items-center bg-white dark:bg-gray-900"> <!-- Flex container to align items horizontally -->
+                <div class="p-6">
+                    <h3 class="font-bold">{{ event.event_title }}</h3>
+                    <span>{{event.category}}</span>
                 </div>
-            </div>
-        </div>
-    </li>
-</ul>
+                <div class="flex items-center flex-col w-full h-full relative">
+                  <span>on: {{ formatDate(event.event_start) }} </span><br />
+                  <img class="w-auto h-auto" :src="`${config.public.supabase.url}/storage/v1/object/public/event_images/${event.photo}`" alt="Event image" />
+                  <div class="w-full px-4 py-2 absolute center bottom-0 bg-gray-500 opacity-80" v-html="countdowns[index]"></div>
+                </div>
+              </div>
+          </li>
+      </ul>
 
     </div>
     <UModal v-model="isDetailsOpen" prevent-close>
@@ -110,6 +105,9 @@ const userName = ref('');
 const PAGE_SIZE = 104; // Define the page size constant
 const config = useRuntimeConfig()
 const user = useSupabaseUser();
+const props = defineProps({
+  venueId: Number,
+});
 const imageUrl = (photoUrl) => {
   return photoUrl.replace('/media/', '');
 };
@@ -127,17 +125,24 @@ const nextPage = () => {
 }
 const fetchAllEvents = async () => {
   try {
-    // const BASE_URL = useRuntimeConfig().public.apiURL;
     const skip = (currentPage.value - 1) * itemsPerPage.value;
     const response = await fetch(`${useRuntimeConfig().public.baseURL}/api/events?skip=${skip}&take=${itemsPerPage.value}`);
     const data = await response.json();
-    events.value = data;
+    
+    if (Array.isArray(data)) {
+      events.value = data;
+    } else {
+      console.error("Unexpected data format:", data);
+      events.value = []; // Assign an empty array to avoid further errors
+    }
+
     console.log("events.value: ", events.value);
     totalItems.value = data.length;
     const totalPagesCount = Math.ceil(totalItems.value / itemsPerPage.value);
     totalPages.value = totalPagesCount;
   } catch (error) {
-    console.error('Error loading venues:', error);
+    console.error('Error loading events:', error);
+    events.value = []; // Assign an empty array on error
   }
 };
 const openDetailsModal = (event: { venue_id: number }) => {
@@ -158,14 +163,6 @@ const fetchVenueDetails = async (venueId: number) => {
         console.error("Failed to fetch venue details:", error);
     }
 }
-// const editDetailsModal = (venue: object) => {
-//   isDetailsOpen.value = true
-//   content.value = venue
-// }
-// const openAddModal = (venue: object) => {
-//   isAddEditOpen.value = true
-//   content.value = venue
-// }
 const openDeleteModal = (venue: object) => {
   isDeleteOpen.value = true
   content.value = venue
@@ -181,12 +178,6 @@ const openAddEventModal = () => {
   isAddEventOpen.value = true
   editMode.value = true
 }
-// const openMapModal = (venue: object, id: Number) => {
-//   isMapOpen.value = true
-//   content.value = venue
-//   editMode.value = true
-//   venueid.value = id
-// }
 const handleCloseModal = () => {
   isMapOpen.value = false
   isAddEditOpen.value = false
@@ -209,11 +200,102 @@ watch(isMapOpen, (newValue: any) => {
 });
 onMounted( async() => {
   // await eventStore.fetchAllEvents();
-  fetchAllEvents();
   userName.value = "John Biddulph";
+  try {
+    fetchAllEvents();
+    events.value = await eventStore.fetchVenueEvents(props.venueId);
+    startCountdowns();
+  } catch (error) {
+    console.error("Error fetching events:", error);
+  }
 });
+
+const countdowns = ref([]);  // Array to store countdowns for each event
+let countdownInterval: number | undefined = undefined;
+
+function calculateCountdown(eventStartDate: string, durationMinutes: number) {
+  const now = new Date(); // Local time
+  const eventDate = new Date(eventStartDate); // Interpreted as UTC if ISO 8601 format
+  const eventEndDate = new Date(eventDate.getTime() + durationMinutes * 60000); // Duration in milliseconds
+
+  const timeDifference = eventDate.getTime() - now.getTime(); // Difference in milliseconds
+
+  if (timeDifference > 0) {
+    // Event hasn't started yet
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    return `<div>
+        <div class="flex flex-row justify-between" id="countdown">
+        <div class="flex flex-col items-center"><span class="big">${days}</span> <small>day${days !== 1 ? 's' : ''}</small></div>
+        <div class="flex flex-col items-center"><span class="big">${hours}</span> <small>hour${hours !== 1 ? 's' : ''}</small></div>
+        <div class="flex flex-col items-center"><span class="big">${minutes}</span> <small>minute${minutes !== 1 ? 's' : ''}</small></div>
+        <div class="flex flex-col items-center"><span class="big">${seconds}</span> <small>second${seconds !== 1 ? 's' : ''}</small></div>
+        </div>
+      </div>`;
+  } else if (now.getTime() <= eventEndDate.getTime()) {
+    // Event is currently happening
+    return `
+      <div class="flex items-center">
+        <div class="pulsing-dot"></div>
+        <span class="ml-2">Event is currently happening</span>
+      </div>`;
+  } else {
+    // Event has ended
+    return `<UBadge color="red" variant="solid">Event has ended</UBadge>`;
+  }
+}
+
+function startCountdowns() {
+  if (Array.isArray(events.value)) {
+    countdowns.value = events.value.map(event => calculateCountdown(event.event_start, event.duration));
+  } else {
+    console.error("Events is not an array:", events.value);
+    countdowns.value = [];
+  }
+
+  countdownInterval = setInterval(() => {
+    if (Array.isArray(events.value)) {
+      countdowns.value = events.value.map(event => calculateCountdown(event.event_start, event.duration));
+    }
+  }, 1000);
+}
+
+onBeforeUnmount(() => {
+  clearInterval(countdownInterval);
+});
+
+function formatDate(dateString: string): string {
+  const date = new Date(dateString);
+
+  const day = date.getUTCDate();
+  const month = date.toLocaleString('en-US', { month: 'long' });
+  const year = date.getUTCFullYear().toString().slice(-2); // Get last two digits of the year
+  const hours = date.getUTCHours().toString().padStart(2, '0');
+  const minutes = date.getUTCMinutes().toString().padStart(2, '0');
+
+  // Format the day with "st", "nd", "rd", or "th"
+  const daySuffix = (day) => {
+    if (day > 3 && day < 21) return 'th'; // 11th, 12th, 13th
+    switch (day % 10) {
+      case 1: return 'st';
+      case 2: return 'nd';
+      case 3: return 'rd';
+      default: return 'th';
+    }
+  };
+
+  return `${day}${daySuffix(day)} ${month} ${year} at ${hours}:${minutes}`;
+}
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="scss">
+.big{
+  font-size: 3rem;
+  font-stretch: extra-condensed;
+  font-weight: 100;
+  color: rgba($color: #000, $alpha: 0.3);
+}
 </style>
