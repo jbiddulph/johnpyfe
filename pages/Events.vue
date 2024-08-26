10996e81ba0d27388b4c73589db4b9a460a50dc6
jbiddulph <john.mbiddulph@gmail.com>
2024-08-26 22:50:25 +0100

    made the countdown stand out  a bit more

--- pages/Events.vue
+++ pages/Events.vue
@@ -20,10 +20,12 @@
                     <h3 class="font-bold">{{ event.event_title }}</h3>
                     <span>{{event.category}}</span>
                 </div>
-                <div class="flex items-center flex-col w-full h-full relative">
-                  <span>on: {{ formatDate(event.event_start) }} </span><br />
-                  <img class="w-auto h-auto" :src="`${config.public.supabase.url}/storage/v1/object/public/event_images/${event.photo}`" alt="Event image" />
-                  <div class="w-full px-4 py-2 absolute center bottom-0 bg-gray-500 opacity-80" v-html="countdowns[index]"></div>
+                <div v-if="userName === user.user_metadata.name">
+                  <div class="flex items-center flex-col w-full h-full relative">
+                    <span>on: {{ formatDate(event.event_start) }} </span><br />
+                    <img class="w-auto h-auto" :src="`${config.public.supabase.url}/storage/v1/object/public/event_images/${event.photo}`" alt="Event image" />
+                    <div class="w-full px-4 py-2 absolute center bottom-[50%] bg-gray-500 opacity-80" v-html="countdowns[index]"></div>
+                  </div>
                 </div>
               </div>
           </li>
@@ -296,6 +298,6 @@ function formatDate(dateString: string): string {
   font-size: 3rem;
   font-stretch: extra-condensed;
   font-weight: 100;
-  color: rgba($color: #000, $alpha: 0.3);
+  color: rgba($color: #fff, $alpha: 0.8);
 }
 </style>
