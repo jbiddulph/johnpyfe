76b228df920209c7a33ca4cedae1685b7070fa06
jbiddulph <john.mbiddulph@gmail.com>
2024-11-19 22:04:02 +0000

    slug

--- layouts/default.vue
+++ layouts/default.vue
@@ -44,7 +44,7 @@
           <!-- <ul v-else-if="!loggedIn" class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"> -->
             <ul v-else-if="!user" class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <NuxtLink @click="toggleMenu" to="/" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-700 md:p-0 dark:text-white md:dark:hover:text-primary-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" aria-current="page">Home</NuxtLink>
+              <NuxtLink @click="toggleMenu" to="/" class="block py-2 px-3 text-white bg-primary-700 rounded md:bg-transparent md:text-primary-700 md:p-0 dark:text-white md:dark:text-primary-500" aria-current="page">Home</NuxtLink>
             </li>
             <li>
               <NuxtLink @click="toggleMenu" to="/login" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-700 md:p-0 dark:text-white md:dark:hover:text-primary-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Login</NuxtLink>
---
+++ pages/venues/[id]/[slug].vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>{{ venue.venuename }}</h1>
+    <p>{{ venue.description }}</p>
+    <!-- Add more venue details here -->
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+const route = useRoute();
+const venue = ref({});
+
+onMounted(async () => {
+  const { id, slug } = route.params;
+  try {
+    const response = await axios.get(`/api/venues/${id}`);
+    venue.value = response.data;
+    setPageMeta();
+  } catch (error) {
+    console.error('Error fetching venue:', error);
+  }
+});
+
+const setPageMeta = () => {
+  usePageMeta({
+    title: venue.value.venuename,
+    meta: [
+      {
+        name: 'keywords',
+        content: venue.value.town || ''
+      }
+    ]
+  });
+};
+
+watch(venue, setPageMeta);
+</script>
