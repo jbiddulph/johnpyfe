7cf337536bc476537f02069bc96d2e7ecdaf3c45
jbiddulph <john.mbiddulph@gmail.com>
2024-11-20 16:39:39 +0000

    set full url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,13 +42,7 @@ export default defineNuxtConfig({
     }
   },
   sitemap: {
-    hostname: process.env.NUXT_PUBLIC_APP_URL ?? "https://lookwhatfound.me",
-    routes: async () => {
-      const { data } = await fetch(`${process.env.NUXT_PUBLIC_API_URL ?? "https://lookwhatfound.me"}/api/venue-urls`)
-        .then(res => res.json());
-      return data.map(venue => `/venues/${venue.slug}`);
-    },
-    path: '/sitemap.xml', // Ensure the sitemap is generated at this path
+    sources: ['https://lookwhatfound.me/api/venue-urls'],
   },
   mapbox: {
     accessToken: 'pk.eyJ1IjoiamJpZGR1bHBoIiwiYSI6ImNscDgzemt0ZzJjNW8ydnM0MXJvNG56NjEifQ.h0CNNEv-Yjgkp4WMjOK9mA'
