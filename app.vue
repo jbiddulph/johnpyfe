3221a5f030147c9a8bd8c8cc4188e1dd6941b564
jbiddulph <john.mbiddulph@gmail.com>
2024-11-21 09:22:38 +0000

    added google site verification

--- app.vue
+++ app.vue
@@ -7,4 +7,12 @@
 </template>
 
 <script setup>
+useHead({
+  meta: [
+    {
+      name: 'google-site-verification',
+      content: 'B3V3n6Nt0V1vu0qZJJzBRb_1B9ZdCMZgTkST-Ug3sLw',
+    },
+  ],
+});
 </script>
