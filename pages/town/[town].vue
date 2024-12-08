9847c3d06b6ae3968a24eb8256118c771939c5c0
jbiddulph <john.mbiddulph@gmail.com>
2024-12-08 10:34:00 +0000

    trying a redeploy as didn't work before

--- pages/town/[town].vue
+++ pages/town/[town].vue
@@ -8,7 +8,7 @@
         class="w-full h-auto object-cover" />
       <h1 class="text-6xl md:text-8xl absolute inset-0 flex items-center justify-center text-white drop-shadow-md">{{ townName }}</h1>
     </div>
-    <div class="container mx-auto p-4">
+    <div class="container mx-auto p-4 my-8">
       <h2 class="text-4xl font-bold my-8">Events in {{ townName }}</h2>
       <ul class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
         <li v-for="(event, index) in events" :key="event.id">
